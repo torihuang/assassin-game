ede4b65242b22787867d335d4f49a2aa6737fe6a
Jones Melton <shavah@gmail.com>
2016-03-26 16:21:13 -0500

    Add statuses to game model

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,6 +3,5 @@ get '/' do
 end
 
 get '/games' do
-  @games = Game.where(status: 'scheduled')
   erb :'/games/index'
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,22 @@ class Game < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
   has_many :enrollments
   has_many :players, through: :enrollments, source: :user
+
+  def self.scheduled_games
+    Game.where(status: 'scheduled')
+  end
+
+  def self.active_games
+    Game.where(status: 'active')
+  end
+
+  def self.completed_games
+    Game.where(status: 'completed')
+  end
+
+  def self.aborted_games
+    Game.where(status: 'aborted')
+  end
+
 end
 
