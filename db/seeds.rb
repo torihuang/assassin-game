ee06303dd52a4f2ec36850e9e72cf42963454be7
Zach Barton & Tori Huang <pair+ZABarton.torihuang@devbootcamp.com>
2016-03-28 20:28:25 -0500

    Add css to user show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ count = 0
     User.create(password: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, nickname: Faker::Superhero.power, email: Faker::Internet.email, image_path: Faker::Avatar.image, bio: Faker::Lorem.paragraph, kill_count: 0, game_count: 0, win_count: 0)
   end
 
-  new_game = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(5), tagline: Faker::Hipster.sentence(3), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now)
+  new_game = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now)
 
   10.times do
     user = User.find(sub_count + 1)
