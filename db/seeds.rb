6a3ad9629327f681bd580e9169afe461e2a07bad
Tori Huang <victoriahuang20@gmail.com>
2016-04-07 10:15:03 -0500

    Add start game button that is only visible to creator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@ count = 0
     User.create(password: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, nickname: Faker::Superhero.power, email: Faker::Internet.email, image_path: Faker::Avatar.image, bio: Faker::Lorem.paragraph, kill_count: 0, game_count: 0, win_count: 0)
   end
 
-  new_game_1 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now)
+  new_game_1 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now, creator_id: 1)
 
-  new_game_2 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now)
+  new_game_2 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now, creator_id: 2)
 
-  new_game_3 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now)
+  new_game_3 = Game.create(status: ["scheduled","active","aborted","completed"].sample, name: Faker::Hipster.sentence(2), tagline: Faker::Hipster.sentence(5), description: Faker::Lorem.paragraph, image_path: Faker::Avatar.image, city: Faker::Address.city, state: Faker::Address.state, start_date: DateTime.now, end_date: DateTime.now, creator_id: 3)
 
   10.times do
     user = User.find(sub_count + 1)
