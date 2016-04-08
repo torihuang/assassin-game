790898a242e26764df0afec44c99f300e90bdd36
Emily <ebosakowski@gmail.com>
2016-04-08 10:10:16 -0500

    Add incrementing stats upon a user kills another player. Change user show page to show stats.

--- app/controllers/enrollment_controller.rb
+++ app/controllers/enrollment_controller.rb
@@ -10,8 +10,6 @@ end
 
 #EB NOTE
 # Currently, it still lists that player's next target from their most recent enrollment on their show page. That's a thing to configure. I tested this code by seeding the DB with 3 users and killing them one after the other, it seems to work. -EB
-
-#changes the player's/killer's enrollment record
 put '/enrollments/:id' do
   @enrollment = Enrollment.find(params[:id])
   killer = @enrollment.user
@@ -24,12 +22,14 @@ put '/enrollments/:id' do
   new_corpse_record = new_corpse.enrollments.find_by(game_id: game.id)
   new_corpse_record.killed_by_id = killer.id
   new_corpse_record.save
+  killer.increment!(:kill_count)
   # binding.pry
   if @enrollment.game.active_player_count > 1
     # continue game
     killer_record = killer.enrollments.find_by(game_id: game.id)
     killer_record.target = new_corpse_record.target
     killer_record.save
+    killer.increment!(:win_count)
     redirect "/users/#{killer.id}"
   else
     # end game
